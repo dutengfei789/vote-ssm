389edd7b5e33773c66312d9c4e2e158c267f9e09
du <dutengfei789@126.com>
2019-02-22 17:45:01 +0800

    This reverts commit da65d8a

---
+++ src/main/java/com/baizhi/votesys/controller/VoteController.java
@@ -0,0 +1,21 @@
+package com.baizhi.votesys.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * InnoDB free: 4096 kB 前端控制器
+ * </p>
+ *
+ * @author 杜腾飞
+ * @since 2019-02-22
+ */
+@Controller
+@RequestMapping("/vote")
+public class VoteController {
+
+}
+
---
+++ src/main/java/com/baizhi/votesys/service/VoteService.java
@@ -0,0 +1,16 @@
+package com.baizhi.votesys.service;
+
+import com.baizhi.votesys.entity.Vote;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * InnoDB free: 4096 kB 服务类
+ * </p>
+ *
+ * @author 杜腾飞
+ * @since 2019-02-22
+ */
+public interface VoteService extends IService<Vote> {
+
+}
