503c0f7b18a8623ae166d00963274cc615bfbec3
adminLi <94365947@qq.com>
2019-02-22 15:31:00 +0800

    Test GitHub

---
+++ src/main/java/com/baizhi/votessm/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.baizhi.votessm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @project: vote-ssm
+ * @author: AdminLi
+ * @date: 2019/2/22
+ */
+@Controller
+public class UserController {
+
+}
+
