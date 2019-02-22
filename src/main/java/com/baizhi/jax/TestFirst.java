004a1cb9920a237180a2791244bdbc6577acdd27
jax <maisel1212@163.com>
2019-02-22 15:58:22 +0800

    from jax!-V1.0

---
+++ src/main/java/com/baizhi/jax/TestFirst.java
@@ -0,0 +1,11 @@
+package com.baizhi.jax;
+
+/**
+ * @aucthor:jjx
+ * @Create:2019-02-22 15:56
+ */
+public class TestFirst {
+    public static void main(String[] args) {
+        System.out.println("i am jax!");
+    }
+}
