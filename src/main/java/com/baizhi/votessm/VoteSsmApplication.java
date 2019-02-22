9d7a8d12a85e85564d701c24b5d3fe9ec9f1ca29
du <dutengfei789@126.com>
2019-02-22 15:15:36 +0800

    Initial commit

---
+++ src/main/java/com/baizhi/votessm/VoteSsmApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.votessm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteSsmApplication.class, args);
+    }
+
+}
