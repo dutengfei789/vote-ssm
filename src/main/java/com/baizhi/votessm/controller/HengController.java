3d89feae30651128ad98c5e1239921a7a86fc01c
jax <maisel1212@163.com>
2019-02-22 16:06:23 +0800

    from jax!-V2.0

--- src/main/java/com/baizhi/votessm/controller/HengController.java
+++ src/main/java/com/baizhi/votessm/controller/HengController.java
@@ -5,5 +5,7 @@ public class HengController {
     public void test() {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
 }
