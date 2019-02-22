1749b398a2da2f3704eac2b0ea51bf3a742a86f3
ysk <1160750252@qq.com>
2019-02-22 16:06:20 +0800

    test version1.0

--- src/main/java/com/baizhi/votessm/controller/UserController.java
+++ src/main/java/com/baizhi/votessm/controller/UserController.java
@@ -2,17 +2,13 @@ package com.baizhi.votessm.controller;
 
 import org.springframework.stereotype.Controller;
 
-/**
- * @project: vote-ssm
- * @author: AdminLi
- * @date: 2019/2/22
- */
 @Controller
 public class UserController {
 
     public void test() {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("this is beijing");
     }
 }
 
