21c6ed12aa8be9b3958f6636d49a8fb010130a50
ysk <1160750252@qq.com>
2019-02-22 17:52:59 +0800

    Item version1.0

---
+++ src/main/java/com/baizhi/votesys/dao/ItemDao.java
@@ -0,0 +1,7 @@
+package com.baizhi.votesys.dao;
+
+import com.baizhi.votesys.entity.Item;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface ItemDao extends BaseMapper<Item> {
+}
