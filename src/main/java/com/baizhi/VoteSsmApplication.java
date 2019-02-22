459bdf50a016a1ef2689a095c5a9e3428dcb6c08
du <dutengfei789@126.com>
2019-02-22 15:35:40 +0800

    更新包结构

---
+++ src/main/java/com/baizhi/VoteSsmApplication.java
@@ -0,0 +1,22 @@
+package com.baizhi;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@MapperScan()
+public class VoteSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteSsmApplication.class, args);
+    }
+
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
